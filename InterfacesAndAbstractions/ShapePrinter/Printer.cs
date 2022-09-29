711ebfba6cf1fbd472845c2c4973505d5104afcb
Aleh Kasiankou <aleh.kasiankou@gmail.com>
2022-09-29 16:27:05 +0300

    Code Refactoring

--- InterfacesAndAbstractions/ShapePrinter/Printer.cs
+++ InterfacesAndAbstractions/ShapePrinter/Printer.cs
@@ -18,35 +18,6 @@ namespace ShapePrinter
             Queue = new List<List<CoordinatesPoint>>();
         }
 
-        private static int CalculateLeftMargin(List<CoordinatesPoint> scheme,
-                int currentPointIndex, out bool isSkip)
-            //the scheme is sorted
-        {
-            var currentPoint = scheme[currentPointIndex];
-            var point = currentPointIndex > 0
-                ? scheme[currentPointIndex - 1]
-                : new CoordinatesPoint(0, 1, currentPoint.Symbol);
-
-            var margin = currentPoint.X - 1;
-
-            CoordinatesPoint closestPointToLeft = new CoordinatesPoint(0, currentPoint.Y, currentPoint.Symbol);
-            isSkip = false;
-
-
-            bool isSameLine = point.Y == currentPoint.Y;
-            bool isLeftFromCurrentPoint = point.X < currentPoint.X;
-            bool isCloser = point.X > closestPointToLeft.X;
-
-            isSkip = isSameLine && point.X == currentPoint.X;
-
-            if (isSameLine && isLeftFromCurrentPoint && isCloser)
-            {
-                closestPointToLeft = point;
-            }
-
-            margin -= closestPointToLeft.X;
-            return margin;
-        }
 
         private static List<CoordinatesPoint> MergeQueue()
         {
@@ -56,74 +27,17 @@ namespace ShapePrinter
                 drawingScheme.AddRange(printableEntity);
             }
 
-            CoordinatesPoint temp;
-            for (int j = 0; j <= drawingScheme.Count - 2; j++)
-            {
-                for (int i = 0; i <= drawingScheme.Count - 2; i++)
-                {
-                    if (drawingScheme[i].Y > drawingScheme[i + 1].Y)
-                    {
-                        temp = drawingScheme[i + 1];
-                        drawingScheme[i + 1] = drawingScheme[i];
-                        drawingScheme[i] = temp;
-                    }
-
-                    else if (drawingScheme[i].Y == drawingScheme[i + 1].Y)
-                    {
-                        if (drawingScheme[i].X > drawingScheme[i + 1].X)
-                        {
-                            temp = drawingScheme[i + 1];
-                            drawingScheme[i + 1] = drawingScheme[i];
-                            drawingScheme[i] = temp;
-                        }
-
-                        else if (drawingScheme[i].X == drawingScheme[i + 1].X)
-                        {
-                            var conflictSymbol = '$';
-                            drawingScheme[i] = new CoordinatesPoint(drawingScheme[i].X, drawingScheme[i].Y, conflictSymbol);
-                            drawingScheme[i + 1] = new CoordinatesPoint(drawingScheme[i + 1].X, drawingScheme[i + 1].Y,
-                                conflictSymbol);
-                        }
-                    }
-                }
-            }
+            drawingScheme = PrintHelper.SortPointsByYAndX(drawingScheme);
 
             return drawingScheme;
         }
 
-        public static List<CoordinatesPoint> AdaptForPrinting(List<CoordinatesPoint> drawingScheme)
-        {
-            var addToX = 0;
-            var addToY = 0;
-            foreach (var point in drawingScheme)
-            {
-                if (point.X < 0 && Math.Abs(point.X) > addToX)
-                {
-                    addToX = (Math.Abs(point.X)) + 1;
-                }
-
-                if (point.Y < 0 && Math.Abs(point.Y) > addToY)
-                {
-                    addToY = (Math.Abs(point.Y)) + 1;
-                }
-            }
-
-            for (int i = 0; i < drawingScheme.Count; i++)
-            {
-                var point = drawingScheme[i];
-                point.X += addToX;
-                point.Y += addToY;
-                // drawingScheme[i] = valueTuple;
-            }
-
-            return drawingScheme;
-        }
 
         public static void Print()
         {
             Console.Clear();
             var drawingScheme = MergeQueue();
-
+            ClearQueue();
 
             var lastPointY = 1;
             if (drawingScheme[0].Y > 1)
@@ -140,7 +54,7 @@ namespace ShapePrinter
                     margin = "\n";
                 }
 
-                var spacesNeeded = CalculateLeftMargin(drawingScheme, index, out bool isSkip);
+                var spacesNeeded = PrintHelper.CalculateLeftMargin(drawingScheme, index, out bool isSkip);
                 if (isSkip)
                 {
                     continue;
