f275ac8bbbd4dd29eaf2b0dd28b641f3189a1687
Aleh Kasiankou <aleh.kasiankou@gmail.com>
2022-10-12 14:06:08 +0300

    Added Stack

--- DataStructures/DataStructures/Program.cs
+++ DataStructures/DataStructures/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using LinkedList.DoublyLinkedList;
 using LinkedList.SinglyLinkedList;
+using Stack;
 
 namespace DataStructures
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            // TestLinkedList();
+            TestStack();
+        }
+
+        internal static void TestLinkedList()
         {
             var testList = new DoublyLinkedList<int>(new[] { 1, 2, 3, 4 });
             Console.WriteLine(testList.ToString());
@@ -26,5 +33,21 @@ namespace DataStructures
             Console.WriteLine(testList.ToString());
             // testList.Insert(25, 100);
         }
+
+        internal static void TestStack()
+        {
+            var testStack = new Stack<int>(new[] { 1, 2, 3, 4 });
+            Console.WriteLine(testStack);
+            testStack.Push(5);
+            Console.WriteLine(testStack);
+            testStack.Push(6);
+            
+            Console.WriteLine(testStack);
+            Console.WriteLine(testStack.Pop());
+            Console.WriteLine(testStack);
+            testStack.Clean();
+            Console.WriteLine(testStack);
+        }
+
     }
 }
