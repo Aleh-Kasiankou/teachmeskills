448efbf3fdcc8e1e2014320dd7acc237ba070024
Aleh Kasiankou <aleh.kasiankou@gmail.com>
2022-11-07 10:08:46 +0300

    Added update functionality (CRUD finished)

--- Crud API APP/AttributeAPI/Models/Attribute/AttributeUpdateRequest.cs
+++ Crud API APP/AttributeAPI/Models/Attribute/AttributeUpdateRequest.cs
@@ -1,7 +1,16 @@
-﻿namespace API.Models.Attribute
+﻿using System.Collections.Generic;
+using API.Entities;
+using API.Models.PossibleValues;
+
+namespace API.Models.Attribute
 {
     public class AttributeUpdateRequest : BaseModel
     {
         
+        public string Name { get;  set; }
+        public AttributeType AttributeType { get; set; }
+        public string DefaultLiteralValue { get; set; } = null;
+        public string MeasurementUnit { get; set; } = null;
+        public IList<PossibleValueCreateRequest> PossibleValues { get; set; } = new List<PossibleValueCreateRequest>();
     }
 }
