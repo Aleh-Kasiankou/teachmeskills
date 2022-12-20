08ac129a59605637682f695eff91e6e23627ed79
Aleh-Kasiankou <aleh.kasiankou@gmail.com>
2022-12-20 15:21:58 +0300

    Added views with request listing
    +Updated Bootstrap

---
+++ MVCHelpDesk/HelpDesk/Controllers/CustomerController.cs
@@ -0,0 +1,19 @@
+﻿using HelpDesk.Persistence;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelpDesk.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly HelpDeskDbContext _dbContext;
+        public CustomerController(HelpDeskDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        // GET
+        public IActionResult Index()
+        {
+            return View(_dbContext.SupportRequests);
+        }
+    }
+}
---
+++ MVCHelpDesk/HelpDesk/Controllers/SupportController.cs
@@ -0,0 +1,24 @@
+﻿using HelpDesk.Persistence;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HelpDesk.Controllers
+{
+    public class SupportController : Controller
+    {
+        private readonly HelpDeskDbContext _dbContext;
+
+        public SupportController(HelpDeskDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // GET
+        public IActionResult Index()
+        {
+            return View(_dbContext.SupportRequests
+                .Include(x => x.SupportDepartment)
+                .Include(x => x.SupportSpecialist));
+        }
+    }
+}
